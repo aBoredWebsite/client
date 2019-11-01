c8625c0e76d3043cbb07595f3a67ec650bddddca
dwitamaalfred <dwitama.alfred@gmail.com>
2019-11-01 10:31:41 +0700

    update index html 10.31

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-$(document).ready(()=>{
-
-
-
-
-})
-
+$(document).ready(function(){
+  $("#button").click(function(){
+      $("#home").animate({
+          height: 'toggle',
+      });
+      $("#question1").show()
+  });
+});
 
 $('#register').on('submit',(e)=>{
   e.preventDefault()
@@ -115,3 +116,50 @@ function signOut() {
     localStorage.removeItem('token')
   });
 }
+
+function generateQuestion(){
+  $.ajax({
+      method: 'GET',
+      url: `https://official-joke-api.appspot.com/random_joke`
+  })
+  .done((data) => {
+    
+      let question = data.setup
+      let punchline = data.punchline
+      let html = ''
+      html += ` <h2 style="color: #464646; text-align: center;font-size: 2.5rem;margin-left: 80px;margin-right: 80px;;">${question}</h2>
+                      <br>
+              <h4 id= "punchline" style="color: #464646; text-align: center;font-size: 2.5rem;margin-left: 80px;margin-right: 80px;font-weight: 200;font-size: 2rem; font-style: italic;display:none;">${punchline}</h4>
+            
+              `
+      $('#randomQuestion').prepend(html)
+  })
+  .fail((err)=>{
+      console.log(err)
+  })
+}
+
+
+
+       let counter = 0
+      $("#answer").click(function(){
+          $("#punchline").show()
+          $("#answer").hide()
+          $("#more").show()
+          // let counter = $("#true").text()
+          // console.log(counter)
+          counter++
+          $("#score").empty()
+          // $("#true").text(counter)
+          $("#score").append(`
+              <p>${counter}</p>
+          `)
+
+      });
+
+      $("#more").click(function(){
+          generateQuestion()
+          $("#answer").show()
+          $("#more").hide()
+          $('#randomQuestion').empty()
+      });
